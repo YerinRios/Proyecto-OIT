07bcd055c083e38741e6e1d2908e082154a0b7ce
Yerin Rios <yerinreal@gmail.com>
2022-09-28 12:11:00 -0500

    Asignacion de clases a label y divs,  select añadidos

--- form_regist/index.php
+++ form_regist/index.php
@@ -11,139 +11,161 @@
     
     <div class="container">
         
-
         <form class="form_class" action="registro.php" name="registro" method="POST" >
             <h1 class="title">Formulario de registro</h1>
         
-            <div>
-                <label for="name">Nombres</label>
+            <div class="input-group">
+                <label class="label" for="name">Nombres</label>
                 <input type="text" name="nombre" id="nombre" required >
             </div>
         
-            <div>       
-                <label for="user">Usuario:</label>
+            <div class="input-group">       
+                <label class="label" for="user">Usuario:</label>
                 <input type="text" name="user" id="user" required>
             </div>     
         
-            <div>
-                <label for="password">Contraseña:</label>
+            <div class="input-group">
+                <label class="label" for="password">Contraseña:</label>
                 <input type="password" name="password" id="password" required>
             </div>
 
-            <div>
-                <label for="nombre1">Primer Nombre</label>
+            <div class="input-group">
+                <label class="label" for="nombre1">Primer Nombre</label>
                 <input type="text" name="nombre1" id="nombre1" required >
             </div>
 
-            <div>
-                <label for="nombre2">Segundo Nombre</label>
+            <div class="input-group">
+                <label class="label" for="nombre2">Segundo Nombre</label>
                 <input type="text" name="nombre2" id="nombre2" required >
-            </div>
+            </div class="input-group">
 
-            <div>
-                <label for="apellido1">Primer Apellido</label>
+            <div class="input-group">
+                <label class="label" for="apellido1">Primer Apellido</label>
                 <input type="text" name="apellido1" id="apellido1" required >
             </div>
 
-            <div>
-                <label for="apellido2">Segundo Apellido</label>
+            <div class="input-group">
+                <label class="label" for="apellido2">Segundo Apellido</label>
                 <input type="text" name="apellido2" id="apellido2" required >
             </div>
 
-            <div>
-                <label for="birthday">Fecha de Nacimiento</label>
+            <div class="input-group">
+                <label class="label" for="birthday">Fecha de Nacimiento</label>
                 <input type="date" name="birthday" id="birthday" required >
             </div>
 
-            <div>
-                <label for="edad">Edad</label>
+            <div class="input-group">
+                <label class="label" for="edad">Edad</label>
                 <input type="number" name="edad" id="edad" required >
             </div>
 
-            <div>
-                <label for="sexo">sexo</label>
-                <input type="text" name="sexo" id="sexo" required >
+            <div class="input-group">
+                <label class="label" for="sexo">sexo</label>
+                    <select class="select" name="sexo" id="sexo" required >
+                        <option disabled selected>Seleccione...</option>
+                        <option value="M">Masculino</option>
+                        <option value="F">Femenino</option>
+                        <option value="NB">No Binario</option>
+                    </select>
             </div>
 
-            <div>
-                <label for="tipo_documento">Tipo de Documento </label>
-                <input type="text" name="tipo_documento" id="tipo_documento" required >
+            <div class="input-group">
+                <label class="label" for="tipo_documento">Tipo de Documento </label>
+                    <select class="select" name="tipo_documento" id="tipo_documento" required>
+                        <option disabled selected>Seleccione...</option>
+                        <option value="CC">CEDULA DE CIUDADANIA</option>
+                        <option value="CEC">CEDULA DE EXTRANGERIA COLOMBIANA</option>
+                        <option value="CE">CEDULA EXTRANGERA</option>
+                        <option value="PAS">PASAPORTE</option>
+                        <option value="REC">REGISTRO CIVIL</option>
+                        <option value="TI">TARGETA DE IDENTIDAD</option>
+                    </select>
             </div>
 
-            <div>
-                <label for="numero_documento">Numero de Documento</label>
+            <div class="input-group">
+                <label class="label" for="numero_documento">Numero de Documento</label>
                 <input type="number" name="numero_documento" id="numero_documento" required >
             </div>
 
-            <div>
-                <label for="ciudad_expedicion">Ciudad de Expedición</label>
+            <div class="input-group">
+                <label class="label" for="ciudad_expedicion">Ciudad de Expedición</label>
                 <input type="text" name="ciudad_expedicion" id="ciudad_expedicion" required >
             </div>
 
-            <div>
-                <label for="pais">Pais</label>
+            <div class="input-group">
+                <label class="label" for="pais">Pais</label>
                 <input type="text" name="pais" id="pais" required >
             </div>
 
-            <div>
-                <label for="departamento">Departamento</label>
+            <div class="input-group">
+                <label class="label" for="departamento">Departamento</label>
                 <input type="text" name="departamento" id="departamento" required >
             </div>
 
-            <div>
-                <label for="ciudad">Ciudad</label>
+            <div class="input-group">
+                <label class="label" for="ciudad">Ciudad</label>
                 <input type="text" name="ciudad" id="ciudad" required >
             </div>
 
-            <div>
-                <label for="barrio">Barrio</label>
+            <div class="input-group">
+                <label class="label" for="barrio">Barrio</label>
                 <input type="text" name="barrio" id="barrio" required >
             </div>
 
-            <div>
-                <label for="telefono">Telefono</label>
+            <div class="input-group">
+                <label class="label" for="telefono">Telefono</label>
                 <input type="number" name="telefono" id="telefono" required >
             </div>
 
-            <div>
-                <label for="celular">Celular</label>
+            <div class="input-group">
+                <label class="label" for="celular">Celular</label>
                 <input type="number" name="celular" id="celular" required >
             </div>
 
-            <div>
-                <label for="correo">Correo Electronico</label>
+            <div class="input-group">
+                <label class="label" for="correo">Correo Electronico</label>
                 <input type="email" name="correo" id="correo" required >
             </div>
 
-            <div>
-                <label for="nivel_academico">Nivel Academico</label>
-                <input type="tetx" name="nivel_academico" id="nivel_academico" required >
+            <div class="input-group">
+                <label class="label" for="nivel_academico">Nivel Academico</label>
+                    <select class="select" name="nivel_academico" id="nivel_academico" required>
+                        <option disabled selected>Seleccione...</option>
+                        <option value="Preescolar">Preescolar</option>
+                        <option value="Primaria">Primaria</option>
+                        <option value="Secundaria">Secundaria</option>
+                        <option value="Bachillerato">Bachillerato</option>
+                        <option value="pregrado">Pregrado</option>
+                        <option value="postgrado">Postgrado</option>
+                        <option value="Maestria">Maestría</option>
+                        <option value="Doctorado">Doctorado</option>
+                    </select>
             </div>
 
-            <div>
-                <label for="profesion">Profesion</label>
+            <div class="input-group">
+                <label class="label" for="profesion">Profesion</label>
                 <input type="tetx" name="profesion" id="profesion" required >
             </div>
 
-            <div>
-                <label for="role">Role</label>
+            <div class="input-group">
+                <label class="label" for="role">Role</label>
                 <input type="tetx" name="role" id="role" required >
             </div>
 
-            <div>
-                <label for="fecha_registro">Fecha de Registro</label>
+            <div class="input-group">
+                <label class="label" for="fecha_registro">Fecha de Registro</label>
                 <input type="date" name="fecha_registro" id="fecha_registro" required >
             </div>
 
-            <div>
-                <label for="fecha_activacion">Fecha de Activación</label>
+            <div class="input-group">
+                <label class="label" for="fecha_activacion">Fecha de Activación</label>
                 <input type="date" name="fecha_registro" id="fecha_registro" required >
             </div>
 
-        <div>
-            <input type="submit" value="Registrarse" name="enviar" >
-            <input type="reset" value="borrar" name="borrar" >
-        </div>
+            <div class="button">
+                <input type="submit" value="Registrarse" name="enviar" >
+                <input type="reset" value="borrar" name="borrar" >
+            </div>
 
         </form>
     </div>
